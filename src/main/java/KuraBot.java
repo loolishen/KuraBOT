73a5168c1c22a8d2980940ea2c0c6faa253b0e30
loolishen <69563443+horikura@users.noreply.github.com>
2023-07-17 16:52:00 +0800

    Initial commit

---
+++ src/main/java/KuraBot.java
@@ -0,0 +1,10 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class KuraBot {
+
+    public static void main(String[] args){
+
+        JDA jda = JDABuilder.createDefault(".").build();
+    }
+}
