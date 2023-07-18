6c5969af358a76682b1c9f0a3fdf896f3fae1f84
loolishen <69563443+horikura@users.noreply.github.com>
2023-07-18 19:16:56 +0800

    Stored the token as an environment variable

--- src/main/java/KuraBot.java
+++ src/main/java/KuraBot.java
@@ -1,10 +1,13 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
+import java.util.Map;
+
 public class KuraBot {
 
     public static void main(String[] args){
+        String token = System.getenv("DISCORD_TOKEN");
+        JDA jda = JDABuilder.createDefault(token).build();
 
-        JDA jda = JDABuilder.createDefault("MTEzMDc3NDg0MDI0NTI0Mzk5NA.GOwvf0.EXqcs0bEkmh3Rx2EzhxYd0nvPao8N6fkmn55mM").build();
     }
 }
