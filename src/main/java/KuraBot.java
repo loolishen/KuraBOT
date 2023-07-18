1cc6d17ab564ad73405dd2720ac22713722ee49a
loolishen <69563443+horikura@users.noreply.github.com>
2023-07-18 18:05:02 +0800

    Starting the bot

--- src/main/java/KuraBot.java
+++ src/main/java/KuraBot.java
@@ -5,6 +5,6 @@ public class KuraBot {
 
     public static void main(String[] args){
 
-        JDA jda = JDABuilder.createDefault(".").build();
+        JDA jda = JDABuilder.createDefault("MTEzMDc3NDg0MDI0NTI0Mzk5NA.GOwvf0.EXqcs0bEkmh3Rx2EzhxYd0nvPao8N6fkmn55mM").build();
     }
 }
