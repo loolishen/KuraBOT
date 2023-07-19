05c2a11dd47eee13b89f6f41eb7ae193718c7c9b
loolishen <69563443+horikura@users.noreply.github.com>
2023-07-19 18:23:28 +0800

    Smol change

--- src/main/java/Event/BotListeners.java
+++ src/main/java/Event/BotListeners.java
@@ -1,21 +1,30 @@
 package Event;
 
+import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.channel.ChannelCreateEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.Objects;
+
 public class BotListeners extends ListenerAdapter {
 
     @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-
-        if (!event.getAuthor().isBot()){
-            String messageSent = event.getMessage().getContentRaw();
-            event.getChannel().sendMessage("This was sent: " + messageSent).queue();
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.isFromType(ChannelType.PRIVATE))
+        {
+            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
+                    event.getMessage().getContentDisplay());
         }
-
+        else
+        {
+            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
+                    event.getChannel().asTextChannel().getName(), Objects.requireNonNull(event.getMember()).getEffectiveName(),
+                    event.getMessage().getContentDisplay());
+        }
     }
 
     @Override
